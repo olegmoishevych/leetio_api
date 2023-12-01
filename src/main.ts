1637588b736c15084ad49c78c578bda2f70bbb96
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-01 21:56:57 +0200

    add process.env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
 
+const PORT = process.env.PORT || 8000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
+  });
 }
 bootstrap();
