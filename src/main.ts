69c4b8cff92088f4291d61a2ae4078807700107f
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 17:00:57 +0200

    install class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const PORT = configService.get('PORT') || 8000;
 
