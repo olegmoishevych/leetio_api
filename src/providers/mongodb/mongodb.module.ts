62417a88b045462d7400b1b0879d3308417c6f4c
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 19:46:32 +0200

    add comments for mongodb module

--- src/providers/mongodb/mongodb.module.ts
+++ src/providers/mongodb/mongodb.module.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
+/**
+ * MongoDB module.
+ * This module sets up and configures the MongoDB connection for the application.
+ * It uses asynchronous factory methods to dynamically configure the database connection
+ * using application settings.
+ */
 @Module({
   imports: [
     MongooseModule.forRootAsync({
