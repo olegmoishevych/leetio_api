ca45fbc46659d23291bfb58bb07d8eab5d83235f
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 19:38:32 +0200

    add comments for registration dto

--- src/modules/auth/api/input-dtos/registration.dto.ts
+++ src/modules/auth/api/input-dtos/registration.dto.ts
@@ -1,6 +1,12 @@
 import { IsString, IsEmail, IsDate, IsOptional } from 'class-validator';
 import { Type } from 'class-transformer';
 
+/**
+ * Data Transfer Object for user registration.
+ * This class defines the structure of data received for user registration,
+ * including validations for each field using class-validator decorators.
+ */
+
 export class RegistrationDto {
   @IsString()
   firstName: string;
