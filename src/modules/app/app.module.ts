71492a19afa5a255fd5a483e289156cc477558f5
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 19:08:07 +0200

    create mongo db provider

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './api/app.controller';
 import { AppService } from './application/app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongodbModule } from '../mongodb/mongodb.module';
+import { MongodbModule } from '../../providers/mongodb/mongodb.module';
 import { AuthModule } from '../auth/auth.module';
 import { JwtModule } from '../jwt/jwt.module';
 
