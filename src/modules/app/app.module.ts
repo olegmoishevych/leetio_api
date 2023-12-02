d741be4b0912d6ea998eab5bd65e48f8ae138ec1
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 16:56:37 +0200

    breaking into structure

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongodbModule } from './mongodb/mongodb.module';
-import { AuthModule } from './auth/auth.module';
-import { JwtModule } from './jwt/jwt.module';
+import { MongodbModule } from '../mongodb/mongodb.module';
+import { AuthModule } from '../auth/auth.module';
+import { JwtModule } from '../jwt/jwt.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), MongodbModule, AuthModule, JwtModule],
