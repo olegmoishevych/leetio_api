70ddc2141ce0a977a4732c13a3846e1d7b5df885
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 17:36:49 +0200

    create comments for auth repository

--- src/modules/auth/infrastructure/auth.repository.ts
+++ src/modules/auth/infrastructure/auth.repository.ts
@@ -8,6 +8,13 @@ import { RegistrationDto } from '../api/input-dtos/registration.dto';
 export class AuthRepository {
   constructor(@InjectModel(Auth.name) private authModel: Model<Auth>) {}
 
+  /**
+   * Creates a new user with the provided registration data.
+   *
+   * @param {RegistrationDto} dto - DTO containing user registration data.
+   * @returns {Promise<Auth>} A Promise that resolves to the created user object.
+   */
+
   async create(dto: RegistrationDto): Promise<Auth> {
     const newUser = new this.authModel(dto);
     return newUser.save();
