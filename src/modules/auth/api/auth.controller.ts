9ec8edf96c05278da3c7740da44aaac829526ba5
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 17:37:24 +0200

    create comments for auth controller

--- src/modules/auth/api/auth.controller.ts
+++ src/modules/auth/api/auth.controller.ts
@@ -7,6 +7,13 @@ import { Auth } from '../domain/schema/auth.schema';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  /**
+   * Handles POST requests to register a new user.
+   *
+   * @param {RegistrationDto} dto - DTO containing user registration data.
+   * @returns {Promise<Auth>} A Promise that resolves to the created user object.
+   */
+
   @Post('registration')
   async registration(@Body() dto: RegistrationDto): Promise<Auth> {
     return this.authService.registration(dto);
