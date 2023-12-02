3cac428382d14bc202db66a21c23374cf115048a
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 16:49:38 +0200

    update auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongodbModule } from './mongodb/mongodb.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongodbModule, AuthModule],
+  imports: [ConfigModule.forRoot(), MongodbModule, AuthModule, JwtModule],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Auth, AuthSchema } from './schema/auth.schema';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    ConfigModule,
     MongooseModule.forFeature([{ name: Auth.name, schema: AuthSchema }]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '60s' },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
