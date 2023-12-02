68da33d270508f4cb784f050cd3f342fad80d968
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 17:22:34 +0200

    create auth query repository

--- src/modules/auth/api/auth.controller.ts
+++ src/modules/auth/api/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from '../application/auth.service';
+import { RegistrationDto } from './input-dtos/registration.dto';
+import { Auth } from '../domain/schema/auth.schema';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('registration')
+  async registration(@Body() dto: RegistrationDto): Promise<Auth> {
+    return this.authService.registration(dto);
+  }
 }
