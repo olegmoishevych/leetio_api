47b6ac287a3f3be29b8cd0690fe24c364479daea
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 16:04:31 +0200

    update mongoose config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongodbModule } from './mongodb/mongodb.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), MongodbModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as process from 'process';
+import { ConfigService } from '@nestjs/config';
 
-const PORT = process.env.PORT || 8000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT') || 8000;
+
   await app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
   });
 }
+
 bootstrap();
