e81c72f0b67ff36036ad0e6ab2be63c7d89ba0dd
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 19:45:42 +0200

    add comments for jwt module

--- src/modules/jwt/jwt.module.ts
+++ src/modules/jwt/jwt.module.ts
@@ -2,6 +2,11 @@ import { Module, Global } from '@nestjs/common';
 import { JwtModule as NestJwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
+/**
+ * Global JWT (JSON Web Token) module.
+ * This module configures and provides JWT functionality application-wide.
+ * It is marked as global, making it available in any module without needing to re-import it.
+ */
 @Global()
 @Module({
   imports: [
