b6a0cf525eeb8b985777cedfc1cbdb7bb534f20c
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 17:35:20 +0200

    create comments for auth service

--- src/modules/auth/application/auth.service.ts
+++ src/modules/auth/application/auth.service.ts
@@ -11,11 +11,22 @@ export class AuthService {
     private readonly authQueryRepository: AuthQueryRepository,
   ) {}
 
+  /**
+   * Registers a new user in the system.
+   * Checks if a user with the same email already exists.
+   * Throws a ConflictException if a user with the same email exists.
+   * Otherwise, creates a new user.
+   *
+   * @param {RegistrationDto} dto - DTO containing user registration data.
+   * @returns {Promise<Auth>} A Promise that resolves to the created user object.
+   * @throws {ConflictException} if a user with the same email already exists.
+   */
+
   async registration(dto: RegistrationDto): Promise<Auth> {
     const user = await this.authQueryRepository.findUserByEmail(dto.email);
 
     if (user) {
-      throw new ConflictException('Пользователь с таким email уже существует');
+      throw new ConflictException('User with the same email already exists');
     }
 
     return this.authRepository.create(dto);
