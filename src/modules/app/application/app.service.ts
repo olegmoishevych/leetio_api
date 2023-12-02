fc7d38dcd00270d2a535b932ef6249374bdc4bdc
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 19:42:49 +0200

    add comments for app service

--- src/modules/app/application/app.service.ts
+++ src/modules/app/application/app.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
+/**
+ * Main application service.
+ * This service contains the business logic of the main application.
+ * Currently, it provides a simple method to return a greeting message.
+ */
 @Injectable()
 export class AppService {
+  /**
+   * Returns a greeting message.
+   * This method can be used to verify basic functionality and connectivity of the application.
+   *
+   * @returns {string} A static greeting message, 'Hello World!'.
+   */
   getHello(): string {
     return 'Hello World!';
   }
