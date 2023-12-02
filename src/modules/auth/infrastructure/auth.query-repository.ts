ba307b66dad10523873f40af8d6578d09f626d87
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 19:34:33 +0200

    add comments for auth query repository

--- src/modules/auth/infrastructure/auth.query-repository.ts
+++ src/modules/auth/infrastructure/auth.query-repository.ts
@@ -3,15 +3,23 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Auth } from '../domain/schema/auth.schema';
 
+/**
+ * Service for querying user authentication data.
+ * This repository utilizes Mongoose models to perform database operations,
+ * specifically targeting user authentication information.
+ */
+
 @Injectable()
 export class AuthQueryRepository {
   constructor(@InjectModel(Auth.name) private authModel: Model<Auth>) {}
 
   /**
-   * Finds a user by their email address.
+   * Retrieves a user entity based on their email address.
+   * This method searches the database for a user with the specified email.
+   * If found, it returns the user's authentication data, otherwise null.
    *
    * @param {string} email - The email address of the user to find.
-   * @returns {Promise<Auth | null>} A Promise that resolves to the user object if found, or null if not found.
+   * @returns {Promise<Auth | null>} A Promise resolved with the user's Auth data or null if no matching user is found.
    */
 
   async findUserByEmail(email: string): Promise<Auth | null> {
