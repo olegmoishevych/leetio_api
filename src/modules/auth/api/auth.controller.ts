4dc1592c6fa4cb56d5b6af3e994e9be1f61b59d0
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 19:37:29 +0200

    fix comments for auth controller

--- src/modules/auth/api/auth.controller.ts
+++ src/modules/auth/api/auth.controller.ts
@@ -3,15 +3,23 @@ import { AuthService } from '../application/auth.service';
 import { RegistrationDto } from './input-dtos/registration.dto';
 import { Auth } from '../domain/schema/auth.schema';
 
+/**
+ * Controller that handles authentication-related requests.
+ * This controller defines endpoints for user authentication operations like registration,
+ * and potentially other CRUD operations for user management in the future.
+ */
+
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   /**
-   * Handles POST requests to register a new user.
+   * Endpoint for user registration.
+   * Accepts a POST request with user registration details and delegates the registration logic to AuthService.
+   * Returns the newly created user's data upon successful registration.
    *
-   * @param {RegistrationDto} dto - DTO containing user registration data.
-   * @returns {Promise<Auth>} A Promise that resolves to the created user object.
+   * @param {RegistrationDto} dto - Data Transfer Object containing the new user's registration information.
+   * @returns {Promise<Auth>} A Promise resolved with the newly registered user's Auth data.
    */
 
   @Post('registration')
