2a3501231c0631296cef0d5671eb7aab0d7ee113
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 19:43:40 +0200

    add comments for app controller

--- src/modules/app/api/app.controller.ts
+++ src/modules/app/api/app.controller.ts
@@ -1,10 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from '../application/app.service';
 
+/**
+ * Main application controller.
+ * This controller handles the root-level requests for the application.
+ * It uses services, like AppService, to process requests and return responses.
+ */
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  /**
+   * Root GET endpoint.
+   * Handles GET requests at the root of the application.
+   * Delegates to AppService to retrieve and return a greeting message.
+   *
+   * @returns {string} The greeting message returned by the AppService.
+   */
   @Get()
   getHello(): string {
     return this.appService.getHello();
