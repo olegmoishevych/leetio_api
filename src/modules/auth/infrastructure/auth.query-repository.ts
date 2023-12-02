652613a9227f5d94e22901432028fc9e8530b4d0
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-02 17:36:06 +0200

    create comments for auth query repository

--- src/modules/auth/infrastructure/auth.query-repository.ts
+++ src/modules/auth/infrastructure/auth.query-repository.ts
@@ -7,6 +7,13 @@ import { Auth } from '../domain/schema/auth.schema';
 export class AuthQueryRepository {
   constructor(@InjectModel(Auth.name) private authModel: Model<Auth>) {}
 
+  /**
+   * Finds a user by their email address.
+   *
+   * @param {string} email - The email address of the user to find.
+   * @returns {Promise<Auth | null>} A Promise that resolves to the user object if found, or null if not found.
+   */
+
   async findUserByEmail(email: string): Promise<Auth | null> {
     return this.authModel.findOne({ email }).exec();
   }
