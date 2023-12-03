50b4f028eb754f415b34253fe46f8cb552a8ec73
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-03 16:30:12 +0200

    add comments for mailer repo

--- src/modules/mailer/infrastructure/mailer.repository.ts
+++ src/modules/mailer/infrastructure/mailer.repository.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 
+/**
+ * MailerRepository: A service class for handling email operations.
+ * This class acts as a layer between the application and the MailerService,
+ * providing a more abstracted interface for sending emails.
+ */
 @Injectable()
 export class MailerRepository {
   constructor(private readonly mailerService: MailerService) {}
+  /**
+   * Sends an email.
+   * Uses the MailerService to send an email with the provided details.
+   *
+   * @param {string} email - The recipient's email address.
+   * @param {string} subject - The subject line of the email.
+   * @param {string} html - The HTML content of the email body.
+   * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating the success or failure of the email sending operation.
+   */
   async sentEmail(
     email: string,
     subject: string,
