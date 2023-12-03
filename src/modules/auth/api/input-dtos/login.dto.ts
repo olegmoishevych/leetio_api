3fd7b421d5c78a3df5b2b41b85185c5f6a915edf
Oleg Moishevych <93545466+Samurai-way@users.noreply.github.com>
2023-12-03 17:03:27 +0200

    upgrade login dto

--- src/modules/auth/api/input-dtos/login.dto.ts
+++ src/modules/auth/api/input-dtos/login.dto.ts
@@ -1,9 +1,10 @@
 import { IsString, IsEmail } from 'class-validator';
 
 /**
- * Data Transfer Object for user registration.
- * This class defines the structure of data received for user registration,
- * including validations for each field using class-validator decorators.
+ * Data Transfer Object (DTO) for user login.
+ * This DTO is used for transferring login data from the client to the server.
+ * It includes properties for user credentials and employs decorators from 'class-validator'
+ * for validating the input data to ensure it meets the required format.
  */
 
 export class LoginDto {
